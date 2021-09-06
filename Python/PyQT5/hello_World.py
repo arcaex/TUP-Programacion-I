608ef27272c9d836dcb5b407c105303befa6cdb8
Jorge <graciajorge.sist@gmail.com>
2021-09-06 20:58:39 -0300

    Update PyQT5

---
+++ Python/PyQT5/hello_World.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtWidgets, QtCore, QtGui, uic
+import sys
+
+template = uic.loadUiType("main.ui")[0]
+
+class Ventana1(QtWidgets.QMainWindow,template):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ventana = Ventana1()
+    ventana.show()
+    sys.exit(app.exec_())
